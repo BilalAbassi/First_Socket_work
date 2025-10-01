dbdb674a106b403ba91f4122d600477f2a3179a2
BilalAbassi <121486012+BilalAbassi@users.noreply.github.com>
2025-10-01 18:48:11 +0500

    new work added

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,48 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { Server } from 'socket.io';
-import http from 'http';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { Server} from "socket.io";
+import http from "http";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// create a connection
 
+// creating a server
+const custom_server = http.createServer(app);
 
-//  socket.io is used for signaling 
-const io = new Server()
+// creating and setting rules for socket.io
+const io = new Server(custom_server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
+// creating a connection
 
+io.on("connection", (socket) => {
+  console.log("user is Connected");
 
-app.listen(4000,()=>{
-    console.log("Server is running on port 4000");
-})
+socket.emit("reply", "hello from server 👋")
 
-io.listen(4001)
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+// get api For testing Purpose
+app.get("/", (req, res) => {
+  res.send({
+    mesg: "working",
+  });
+});
+
+//  socket.io is used for signaling
+custom_server.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
+
+// io.listen(4001)
